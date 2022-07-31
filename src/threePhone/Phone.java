c66f9eb92853ad0fd9e3d4372bd6877561361fbc
KennyShow <thoroughgoingkill@gmail.com>
2022-07-31 23:17:53 +0800

    finish 3

--- src/threePhone/Phone.java
+++ src/threePhone/Phone.java
@@ -1,20 +1,19 @@
+package threePhone;
+
 import java.util.Scanner;
 
-public class threePhone {
+public class Phone {
 
     public static void main(String[] args) {
 //        練習題3: 手機選號:根據使用者輸入的手機號來確認使用者實際支付的價格
+//        如果尾數為8，需支付辦卡費50元
+//        如果尾數為4，需支付辦卡費用0元
+//        如果是其他尾號,需支付辦卡費用20員
         System.out.println("請輸入你的門號號碼");
         String phone = new Scanner(System.in).nextLine();
+        fPhone fPhone = new fPhone();
+        fPhone.getPhone(phone);
 
-        if (phone.length() != 10) {
-            System.out.println("輸入錯誤");
-            return;
-        } else if (phone.charAt(10) == 8) {
-            int iprice = 80;
-        } else if (phone.charAt(10) == 4) {
-            int wprice = 0;
-            
-        }
     }
+
 }
