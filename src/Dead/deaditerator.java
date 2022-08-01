eccf67a8b4521f7e156066baaf58fe34363f8b0b
KennyShow <thoroughgoingkill@gmail.com>
2022-08-01 16:11:02 +0800

    完成雞兔同龍

---
+++ src/Dead/deaditerator.java
@@ -0,0 +1,14 @@
+package Dead;
+
+public class deaditerator {
+
+    public static void main(String[] args) {
+//        for (int i = 1; ; i++) {
+//            System.out.println("加油" + i);
+//        }
+        while (true) {
+            System.out.println("ㄏ ");
+        }
+    }
+
+}
